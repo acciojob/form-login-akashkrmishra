9f8c663dacac0f261691aa6bb4153f5efc29ae6a
akashkrmishra <142215838+akashkrmishra@users.noreply.github.com>
2023-12-20 20:04:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ function getFormvalue() {
     //Write your code here
 	const firstname = document.getElementByName('fname')[0].value;
 	const lastname  = document.getElementByName('lname')[0].value;
-	 console.log(`First name: ${firstName}\nLast name: ${lastName}`);
+	alert(`First name: ${firstName}\nLast name: ${lastName}`);
 	
 }
